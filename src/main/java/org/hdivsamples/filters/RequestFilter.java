763849d6f742b5513b3a36816ec3d2db1b20745f
Xabier Aldama <xabier@hdivsecurity.com>
2019-05-06 18:24:57 +0200

    Changed get user ip method into filter

--- src/main/java/org/hdivsamples/filters/RequestFilter.java
+++ src/main/java/org/hdivsamples/filters/RequestFilter.java
@@ -13,9 +13,14 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import org.hdivsamples.controllers.ActivityController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class RequestFilter implements Filter {
+
+	Logger logger = LoggerFactory.getLogger(ActivityController.class);
 
 	Map<String, String> userAddrMap = new ConcurrentHashMap<>();
 
@@ -34,15 +39,24 @@ public class RequestFilter implements Filter {
 		if (principal != null) {
 			String username = principal.getName();
 			String userAddr = userAddrMap.get(username);
+			String currentUserIP = getUserIP(httpRequest);
 			if (userAddr != null) {
 
-				if (!userAddr.equals(request.getRemoteAddr())) {
+				if (!userAddr.equals(currentUserIP)) {
 					// User Ip has changed. Invalidate user and redirect to login
+					logger.error("Different user IP detected. The session will be invalidated. ");
 					SecurityContextHolder.getContext().setAuthentication(null);
+				}
+				else if (logger.isDebugEnabled()) {
+					logger.debug("Valid Ip " + currentUserIP);
 				}
 			}
 			else {
-				userAddrMap.put(username, getUserIP(httpRequest));
+				if (logger.isDebugEnabled()) {
+					logger.debug("New Ip for user:" + currentUserIP);
+				}
+
+				userAddrMap.put(username, currentUserIP);
 			}
 
 		}
@@ -51,11 +65,12 @@ public class RequestFilter implements Filter {
 	}
 
 	private String getUserIP(final HttpServletRequest request) {
-		if (request.getHeader("X-Forwarded-For") == null) {
-			return request.getRemoteAddr();
+
+		if (request.getHeader("X-Forwarded-For") != null) {
+			return request.getHeader("X-Forwarded-For");
 		}
 		else {
-			return request.getHeader("X-Forwarded-For");
+			return request.getRemoteAddr();
 		}
 	}
 
