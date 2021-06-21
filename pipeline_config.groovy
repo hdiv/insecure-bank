5c46941c9c436226203b6248e45ba3e8ea80198d
Karn Chahar <karn@synopsys.com>
2021-06-21 14:29:39 -0400

    new

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,4 +1,15 @@
 libraries {
-	github
-	whitesource
+	github {
+	   source_type = "github"
+	}
+	
+	maven {
+	   mavenID = "maven"
+	}
+	
+	whitesource {
+	   ws_product = "WhiteSource_Test" 
+    	   ws_project = "Test_jenkins"
+           ws_config  = "ws_mvn.config"
+	}
 }
