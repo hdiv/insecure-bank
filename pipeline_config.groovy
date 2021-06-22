4ad1e5ea82e9045ab616e914b6fe29b53b95d9fa
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-06-22 12:45:40 -0400

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,13 +1,5 @@
 libraries {
-	git {
-	   github
-	   on_commit
-	   on_merge
-	}
-	maven {
-	   mavenID = "maven"
-	}
-	whitesource {
+	   whitesource {
 	   Product = "WhiteSource_Test" 
     	   Project = "Test_jenkins"
            Package_Manager = "maven"
