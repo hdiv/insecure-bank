c71c3aff389b6c8d8ab8eb753c784b6e3054ad16
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-08-19 15:57:59 -0400

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,46 +1,39 @@
 jte {
-  pipeline_template = "java"
+	pipeline_template = "java"
 }
 
 libraries {
-  maven {
-    maven {
-      	Phases = ["clean"]
-	//Maven_Properties = ["skipTests":"true"]
-	Stage = "on_commit-test"
-	version = "3.8.1"
-	//artifact_version = "1.0.1" test
-	//settings = "settings.xml"
-	//modules = "test1,test2,core"b
-    }
+	maven {
+		maven {
+			Phases = ["clean"]
+			//Maven_Properties = ["skipTests":"true"]
+			Stage = "on_commit-test"
+			version = "3.8.1"
+			//artifact_version = "1.0.1" test
+			//settings = "settings.xml"
+			//modules = "test1,test2,core"b
+		}
   
-    aws {
-      defaultregion = "us-east-1"
-      codeartifact_action = "get-authorization-token"
-      codeartifact_domain = "test"
-      codeartifact_domain_owner = "111111115571"
-      codeartifact_query = "authorizationToken"
-      codeartifact_output = "text"
+		aws {
+			defaultregion = "us-east-1"
+			codeartifact_action = "get-authorization-token"
+			codeartifact_domain = "test"
+			codeartifact_domain_owner = "111111115571"
+			codeartifact_query = "authorizationToken"
+			codeartifact_output = "text"
+		}
   	}
-  }
-/*  
-  checkmarx {
-    ProjectName = "CxServer\\SP\\Solarwinds\\MSP_N-Central\\Test_project"
-    Language = "Java"
-    CxCred  = "Cx-Access"
-  }
-  
-  whitesource {
-    Product = "WhiteSource_Test"
-    Project = "Test_jenkins_local"
-    User_Key = "user_key"
-  }
-  */
-  trivy {
-    Opt_In = "no"
-  }
-  
-	checkov {
-		Report = "iac.json"		
+	/*  
+	checkmarx {
+	ProjectName = "CxServer\\SP\\Solarwinds\\MSP_N-Central\\Test_project"
+	Language = "Java"
+	CxCred  = "Cx-Access"
 	}
+  
+	whitesource {
+	Product = "WhiteSource_Test"
+	Project = "Test_jenkins_local"
+	User_Key = "user_key"
+	}
+	*/
 }
